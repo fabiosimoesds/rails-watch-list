0a18755215db087d56eb6cafc350d5d720470abc
fabiosimoesds <fabio.simoesds@gmail.com>
2022-09-16 20:33:00 +0100

    Getting Hash from the API

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-const apiKey = "TMBD_KEY";
+const apiKey = 'e6caf363437a9a502062a3a6f4a68627';
+// const apiKey = TMBD_KEY;
 // Connects to data-controller="search-movies"
 export default class extends Controller {
   static targets = ['list', 'query']
@@ -8,13 +9,16 @@ export default class extends Controller {
   connect() {
     console.log('Hello Search')
     console.log(this.listTarget)
+    console.log(process.env)
   }
 
   search(event) {
     event.preventDefault()
-    console.log(this.queryTarget.value)
     const query = this.queryTarget.value
-    console.log(apiKey)
-    // fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMBD_KEY}&language=en-US&query=${query}&page=1&include_adult=false`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
   }
 }
