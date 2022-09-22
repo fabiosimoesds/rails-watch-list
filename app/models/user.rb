01aaeceaeca9b07bbfc2be21ff0d31ade22e7d73
fabiosimoesds <fabio.simoesds@gmail.com>
2022-09-22 18:02:50 +0100

    add the module confirmable to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :lists
+  has_many :lists, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 end
