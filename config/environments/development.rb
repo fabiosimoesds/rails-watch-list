057b955071c23cf74f741168a25f364975805ef2
fabiosimoesds <fabio.simoesds@gmail.com>
2022-09-28 12:42:31 +0100

    Adding Mailer to the app for email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,17 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDMAIL_USERNAME'],
+    password: ENV['SENDMAIL_PASSWORD'],
+    domain: ENV['MAIL_HOST'],
+    address: 'smtp.gmail.com',
+    port: '587',
+    authentication: :plain,
+    enalbe_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -38,9 +48,12 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  # To enable to send e-mail
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
