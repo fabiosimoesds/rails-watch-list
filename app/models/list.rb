fccc8fb06c5de51f1b6350ec00d75846b544e505
fabiosimoesds <fabio.simoesds@gmail.com>
2022-10-27 16:39:44 +0100

    Added CSV download to Lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,15 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, presence: true
   has_one_attached :photo
+
+  def self.to_csv
+    attributes = %w{id name created_at updated_at}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << user.attributes.values_at(*attributes)
+      end
+    end
+  end
 end
